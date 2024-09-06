0e6791d3dd63724b1ea7ef2a1edb2b07cd1f5a2a
atharvawagh123 <146693478+atharvawagh123@users.noreply.github.com>
2024-09-06 17:08:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: ['https://client1-dmrp.onrender.com', 'https://client1-gamma.vercel.app'],  // Frontend URLs
+    origin: ['https://client1-dmrp.onrender.com', 'https://client1-gamma.vercel.app', 'http://localhost:3000'],  // Frontend URLs
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
